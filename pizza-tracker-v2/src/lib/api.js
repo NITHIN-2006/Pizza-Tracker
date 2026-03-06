b0dac91acc97088346438dd7fdd39845c0e896fb
NITHIN-2006 <budumurinithin@gmail.com>
2026-03-06 16:51:26 +0530

    New Changes

--- pizza-tracker-v2/src/lib/api.js
+++ pizza-tracker-v2/src/lib/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: 'http://localhost:3001',
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-})
+});
+
 
 export const getOptions = async () => {
   const { data } = await api.get('/api/options')
